15a03ee2177f79470be2772f6aa889610dc2e338
unknown <djoako22@gmail.com>
2022-11-23 17:24:37 -0300

    Update

--- api/config.js
+++ api/config.js
@@ -2,7 +2,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let URL_DB = "";
 if (process.env.NODE_ENV === 'dev') {
-    URL_DB = "mongodb://mongo:CnJde9YUZY9wwLRps0g2@containers-us-west-68.railway.app:7430";
+    URL_DB = "";
 } else {
     URL_DB = ""
 };
